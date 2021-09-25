14b0978826ebba66a1b2e5c2163c1577d9176250
Reinaldo Ferro <reinaldo.ferro@gmail.com>
2021-09-25 20:11:11 +0200

    master

--- src/components/RepositoryCounter.jsx
+++ src/components/RepositoryCounter.jsx
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+
 export function RepositoryCounter() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
   return (
     <>
-      <h3>0</h3>
-      <button type="button">Increment</button>
+      <h3>{counter}</h3>
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
     </>
   );
 }
